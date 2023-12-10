7487f7818f9692f9b93db4945d7be06cbbd05a56
Adam Surek <26888201+SuRektM8@users.noreply.github.com>
2023-12-10 09:55:45 -0500

    Completed day one

--- AdventOfCode_2023/Day_One/DayOne.cs
+++ AdventOfCode_2023/Day_One/DayOne.cs
@@ -1,9 +1,39 @@
-﻿namespace AdventOfCode_2023.Day_One;
+﻿using System.Collections.Immutable;
+using System.Net.Mime;
+using System.Reflection;
 
-public class DayOneMain
+namespace AdventOfCode_2023.Day_One;
+
+public static class DayOne
 {
-	static void ()
+	public static void RunDayOne()
 	{
+		string[] inputData = ReadCalibrationDocument();
+		int[] calibrationValues = ExtractCalibrationValues(inputData);
+
+		Console.Write($"Calibration Sum Value: {calibrationValues.Sum()}");
+	}
+
+	private static string[] ReadCalibrationDocument()
+	{
+		string folder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+		string filePath = Path.Combine(folder, @"Day_One\input\CalibrationDoc");
 		
+		return File.ReadAllLines(filePath);
+	}
+
+	private static int[] ExtractCalibrationValues(string[] fileContents)
+	{
+		int[] calValues = new int[fileContents.Length];
+		char[] temp;
+		
+		for (int x = 0; x < fileContents.Length; x++)
+		{
+			IList<char> intChars = fileContents[x].Where(Char.IsDigit).ToList();
+			temp = new[] { intChars[0], intChars[^1] };	
+			calValues[x] = int.Parse(new String(temp.ToArray()));
+		}
+
+		return calValues;
 	}
 }
